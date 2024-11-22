877072a21c6a43ae2d9458d83f71b6c8e53a3b99
abhinavlalam <abhnvllm@gmail.com>
2024-11-23 00:04:17 +0530

    updates

--- automatic-grader-with-langchain.py
+++ automatic-grader-with-langchain.py
@@ -1,4 +1,9 @@
+# cannot run this as there is no free api from OpenAI
+
 from langchain.chat_models import ChatOpenAI
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
 from langchain.schema import BaseOutputParser
+
+from sk import my_sk #importing secret key from another python file
+
--- forescasting-using-transformers.py
+++ forescasting-using-transformers.py
@@ -1,1 +1,2 @@
 # Link - https://medium.com/intel-tech/how-to-apply-transformers-to-time-series-models-spacetimeformer-e452f2825d2e
+
